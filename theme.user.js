00b8d71c2d8da7ea91c703fb97e119dac99d8ee2
Firehawke <Firehawke@gmail.com>
2020-11-21 19:51:29 -0700

    Corrected comment links (Version 1.01)

--- theme.user.js
+++ theme.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name              Hacker News - Dark Theme FH
+// @author            Firehawke, dparpyani
 // @namespace         https://github.com/firehawke
 // @description       Another dark theme for Hacker News, FH Edition
 // @include           https://news.ycombinator.com*
 // @require           https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @grant             none
-// @version           1.0
+// @version           1.01
 // ==/UserScript==
 
 var loadScript = function (src, callback) {
@@ -23,7 +24,7 @@ function start() {
 
   var config = {
     textAreaBg: '#101010',
-    textAreaColor: '#FFEC00',
+    textAreaColor: '#F87431',
     textAreaLeftBorder: '12px solid #CCCCCC',
     inputColor: 'black',
     inputHoverOnBg: '#CCCCCC',
@@ -36,6 +37,7 @@ function start() {
     topBarText: '&nbsp;',
     topBarColor: '#2B2B2B',
     topBarLinkColor: '#FF6300',
+    topBarTitleColor: '#DDDDDD',
     bodyBg: '#2B2B2B',
     centreBg: '#1A1A1A',
     commonBorderSpacing: '1px solid #1A1A1A',
@@ -67,8 +69,23 @@ function start() {
   myJQuery('a:link').css('color', config.fontColor);
 
   //// Comments
+  //// This likely will need some adjusting later to handle
+  //// different levels of moderation. These APPEAR to show up in
+  //// the CSS file at https://news.ycombinator.com/news.css as the following:
+
+  //// .c00, .c00 a:link { color:#000000; }
+  //// .c5a, .c5a a:link, .c5a a:visited { color:#5a5a5a; }
+  //// .c73, .c73 a:link, .c73 a:visited { color:#737373; }
+  //// .c82, .c82 a:link, .c82 a:visited { color:#828282; }
+  //// .c88, .c88 a:link, .c88 a:visited { color:#888888; }
+  //// .c9c, .c9c a:link, .c9c a:visited { color:#9c9c9c; }
+  //// .cae, .cae a:link, .cae a:visited { color:#aeaeae; }
+  //// .cbe, .cbe a:link, .cbe a:visited { color:#bebebe; }
+  //// .cce, .cce a:link, .cce a:visited { color:#cecece; }
+  //// .cdd, .cdd a:link, .cdd a:visited { color:#dddddd; }
+
   myJQuery('.c00').css('color', config.fontColor);
-  myJQuery('.c00 a:link').css('color', config.linkColor);
+  myJQuery('.comment a:link').css('color', config.linkColor);
 
   myJQuery('.comhead, .subtext').css('color', config.comheadDefaultColor);
   myJQuery('.subtext > a').css('color', config.comheadLinksColor);
@@ -77,6 +94,10 @@ function start() {
   myJQuery('.pagetop:eq(1)').children().css('color', 'yellow'); // Login button
 
   myJQuery('.storylink').css('color', config.storyTitle);
+
+  //// Top Bar
+  myJQuery('.pagetop > a').css('color', config.topBarLinkColor);
+  myJQuery('.hnname > a').css('color', config.topBarTitleColor);
 
   //// Spacing
   myJQuery('td').css('border', config.commonBorderSpacing);
@@ -102,7 +123,6 @@ function start() {
 
     //// Text Colors
     myJQuery('.comhead > a').css('color', config.comheadLinksColor);
-    myJQuery('.pagetop a:link').css('color', config.topBarLinkColor);
 
     //// Bar at top
     myJQuery('tr:eq(0)').next().html(config.topBarText);
